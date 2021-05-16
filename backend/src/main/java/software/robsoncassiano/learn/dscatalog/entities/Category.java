08b034884001c58f83eaad2cac15efc9bdfb19da
Robson Cassiano <rcassiano@ciandt.com>
2021-05-16 18:00:48 -0300

    Pom dependencies and Entities

--- backend/src/main/java/software/robsoncassiano/learn/dscatalog/entities/Category.java
+++ backend/src/main/java/software/robsoncassiano/learn/dscatalog/entities/Category.java
@@ -1,8 +1,15 @@
 package software.robsoncassiano.learn.dscatalog.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Category implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String  name;
 

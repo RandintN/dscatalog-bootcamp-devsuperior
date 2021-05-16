05b87072d4986ba93e7d91260972effbd911fc96
Robson Cassiano <rcassiano@ciandt.com>
2021-05-16 11:38:21 -0300

    Project Created

---
+++ backend/src/test/java/software/robsoncassiano/learn/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package software.robsoncassiano.learn.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
